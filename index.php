3fa34086e8b63153394c6719458b3430434b572a
Peter Blum <Peter.Blum@emc.com>
2016-07-15 06:29:08 -0400

    Kitty was born

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html >
+  <body>
+    <h2 style="text-align: center; color: #2B5663; font-size: 2em; font-family: Verdana,sans-serif; font-variant: small-caps;">Spectacular EMC Dojo App! :)</h2>
+    <img style="border-radius: 30px; border: 15px solid #45B0FE; display: block; margin: 50px auto auto auto" src="http://lorempixel.com/g/600/400/cats/" alt="Cute Cat" style="width:600px;height:800px;"><br/>
+  <div style="text-align: center; color: #45B0FE; font-size: 1em; font-family: Verdana,sans-serif;"><?php
+  echo 'My environment variables are: ' .$_ENV["VCAP_SERVICES"] . '!';
+  echo 'Instance ID is : ' .$_ENV["CF_INSTANCE_INDEX"];
+
+  ?>
+  <form name="form" method="post">
+    <input type="text" name="text_box" size="50"/>
+    <input type="submit" id="search-submit" value="submit" />
+  </form>
+  <?php
+    $filepath = json_decode($_ENV["VCAP_SERVICES"],true)['EMC-Persistence'][0]['volume_mounts'][0]['container_path'];
+    $filepath = $filepath . "/test.txt";
+    if(!empty($_POST))
+    {
+       $fh = fopen($filepath, 'a') or die("can't open file");
+       fwrite($fh, $_POST['text_box'] . "\n");
+       fclose($fh);
+    }
+    $fh = fopen($filepath, 'r') or die("can't open file");
+    $file = file_get_contents($filepath, true);
+    echo $file;
+  ?>
+  </div>
+  </body>
+</html>
