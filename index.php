e839619a85282644ed01b7abe07b98e0e912a6a1
Peter Blum <Peter.Blum@emc.com>
2016-07-15 08:53:43 -0400

    Service Name is obtained from ENV["CF_SERVICE"]

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
     <input type="submit" id="search-submit" value="submit" />
   </form>
   <?php
-    $filepath = json_decode($_ENV["VCAP_SERVICES"],true)['EMC-Persistence'][0]['volume_mounts'][0]['container_path'];
+    $service_broker = $_ENV["CF_SERVICE"];
+    $filepath = json_decode($_ENV["VCAP_SERVICES"],true)[$service_broker][0]['volume_mounts'][0]['container_path'];
     $filepath = $filepath . "/test.txt";
     if(!empty($_POST))
     {
